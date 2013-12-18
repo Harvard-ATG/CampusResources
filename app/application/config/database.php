fa57c46b69e94d71fb01a724c0bdcb727bc544d5
Arthur Barrett <abarrett@fas.harvard.edu>
2013-12-18 16:25:13 -0500

    get sqlite working

--- app/application/config/database.php
+++ app/application/config/database.php
@@ -48,11 +48,11 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '';
+$db['default']['hostname'] = 'sqlite:./db/campus_resources.sqlite'; # 
 $db['default']['username'] = '';
 $db['default']['password'] = '';
-$db['default']['database'] = 'campus_resources.db';
-$db['default']['dbdriver'] = 'sqlite';
+$db['default']['database'] = '';
+$db['default']['dbdriver'] = 'pdo';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
