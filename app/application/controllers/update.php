1ac1768002c9d7c2970aa25ed4c2c39db6d60b2e
Arthur Barrett <abarrett@fas.harvard.edu>
2014-01-07 11:41:02 -0500

    fix all upload paths

--- app/application/controllers/update.php
+++ app/application/controllers/update.php
@@ -5,17 +5,17 @@ if (!defined('BASEPATH'))
 
 class Update extends CI_Controller {
       
-    function __construct()
-    {
-        parent::__construct();
+	function __construct()
+	{
+		parent::__construct();
 		$this->load->helper(array('form', 'url', 'download'));
 		$this->load->model('link', '', TRUE);
-    }
+	}
 
-    public function index()
-    {
+	public function index()
+	{
 		$this->load->view('update_form', array('error' => ' ' ));
-    }
+	}
 
 	function downloadLinks()
 	{
@@ -24,33 +24,32 @@ class Update extends CI_Controller {
 		force_download($name, $output);
 	}
 
-    function do_upload()
-    {
+	function do_upload()
+	{
+		$upload_path = dirname(dirname(dirname(realpath(__FILE__)))).'/upload';
 
-        $config['upload_path'] = dirname(dirname(dirname(realpath(__FILE__)))).DIRECTORY_SEPARATOR.'upload';
-        $config['allowed_types'] = '*';
-       	$config['max_size']	= '1024';
+		$config['upload_path'] = $upload_path;
+		$config['allowed_types'] = '*';
+		$config['max_size']	= '1024';
 
 		$this->load->library('upload', $config);
 		$this->upload->initialize($config);
 
 
-        if ( ! $this->upload->do_upload())
+		if (!$this->upload->do_upload())
 		{
-	  		$error = array('error' => $this->upload->display_errors());
- 	  		$this->load->view('update_form', $error);
-        }
+			$error = array('error' => $this->upload->display_errors());
+			$this->load->view('update_form', $error);
+		}
 		else
 		{
 			/* Upload successful! Now try updating database */
-	    	$data = array('upload_data' => $this->upload->data());
-	    	
-	    	// First back up data in the case that the user submits a bad file.
-			$output = $this->link->returnAllLinksAsCSV();
-			file_put_contents("upload/backupCSV.csv", $output);
+			$data = array('upload_data' => $this->upload->data());
+			
+			// First back up data in the case that the user submits a bad file.
+			file_put_contents("$upload_path/backupCSV.csv", $this->link->returnAllLinksAsCSV());
 			
 			$uploadedData = $this->upload->data();
-			//$uploadedData['file_name']
 			if ($this->link->updateLinks($uploadedData['file_name']) == 0)
 			{
 				$data['upload_data'] = "SUCCESS";
@@ -60,9 +59,8 @@ class Update extends CI_Controller {
 				$this->link->updateLinks("backupCSV.csv");
 				unlink("upload/backupCSV.csv");
 			}
-				
-	    	
-            $this->load->view('update_success', $data);
+			
+			$this->load->view('update_success', $data);
 		}
     }
 }
