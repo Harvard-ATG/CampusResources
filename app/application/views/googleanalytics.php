12f5f8dabdc4682376d129dae9dbdbcb968ccdc8
Arthur Barrett <abarrett@fas.harvard.edu>
2014-02-04 14:48:31 -0500

    update google analytics tracking for gsas (different code to separate out the stats)

--- app/application/views/googleanalytics.php
+++ app/application/views/googleanalytics.php
@@ -4,6 +4,6 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-46595028-1', 'harvard.edu');
+  ga('create', 'UA-46595028-2', 'harvard.edu');
   ga('send', 'pageview');
 </script>
